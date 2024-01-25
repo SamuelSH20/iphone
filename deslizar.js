a815726dbb95e36f5b6842c4e1aaec8164acb224
SamuelSH20 <157252437+SamuelSH20@users.noreply.github.com>
2024-01-25 15:47:17 -0500

    Add files via upload

---
+++ deslizar.js
@@ -0,0 +1,16 @@
+// principal.js
+function explorar() {
+    // Lógica para explorar
+    alert("Explorando...");
+}
+
+function verOfertas() {
+    // Lógica para ver ofertas
+    alert("Viendo ofertas...");
+}
+
+function verCatalogo() {
+    // Lógica para ver el catálogo
+    alert("Viendo el catálogo...");
+}
+
